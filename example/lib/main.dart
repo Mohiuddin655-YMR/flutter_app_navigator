4128c83f0a8811ecfe4c9e6f14069a58452e2b05
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-07-06 18:46:15 +0600

    init

---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Navigator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Home'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app_navigator.dart
@@ -0,0 +1,83 @@
+library app_navigator;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppNavigator {
+  final BuildContext context;
+
+  const AppNavigator._(this.context);
+
+  factory AppNavigator.of(BuildContext context) => AppNavigator._(context);
+
+  void go(
+    String route, {
+    Object? extra,
+    String path = "",
+    Map<String, dynamic> queryParams = const <String, dynamic>{},
+  }) {
+    if (path.isNotEmpty || queryParams.isNotEmpty) {
+      if (kIsWeb) {
+        context.goNamed(
+          route,
+          extra: extra,
+          pathParameters: {"name": path},
+          queryParameters: queryParams,
+        );
+      } else {
+        context.pushNamed(
+          route,
+          extra: extra,
+          pathParameters: {"name": path},
+          queryParameters: queryParams,
+        );
+      }
+    } else {
+      if (kIsWeb) {
+        context.go(route, extra: extra);
+      } else {
+        context.push(route, extra: extra);
+      }
+    }
+  }
+
+  void goHome(
+    String route, {
+    String path = "",
+    Object? extra,
+    Map<String, dynamic> queryParams = const <String, dynamic>{},
+  }) {
+    if (path.isNotEmpty || queryParams.isNotEmpty) {
+      if (kIsWeb) {
+        Router.neglect(context, () {
+          context.goNamed(
+            route,
+            extra: extra,
+            pathParameters: {"name": path},
+            queryParameters: queryParams,
+          );
+        });
+      } else {
+        context.pushReplacementNamed(
+          route,
+          extra: extra,
+          pathParameters: {"name": path},
+          queryParameters: queryParams,
+        );
+      }
+    } else {
+      if (kIsWeb) {
+        Router.neglect(context, () {
+          context.go(route, extra: extra);
+        });
+      } else {
+        context.pushReplacement(route, extra: extra);
+      }
+    }
+  }
+
+  void goBack([Object? result]) {
+    context.pop(result);
+  }
+}
